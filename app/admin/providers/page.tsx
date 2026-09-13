29fe06156dbed34854b89fc37670985d4071dc96
pra-y104 <lordsprayeromoarelojie@gmail.com>
2026-09-13 20:29:02 +0100

    Create AdminProvidersPage component

---
+++ app/admin/providers/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminProvidersPage() {
+  return (
+    <main>
+      <h1>Providers</h1>
+      <p>Manage legitimate advertising provider integrations.</p>
+    </main>
+  );
+}
