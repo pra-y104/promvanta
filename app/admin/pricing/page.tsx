a0cffaa380f8a3dc24c3c6a023c1f0a97be6afec
pra-y104 <lordsprayeromoarelojie@gmail.com>
2026-09-13 20:25:56 +0100

    Add Admin Pricing Page component

---
+++ app/admin/pricing/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPricingPage() {
+  return (
+    <main>
+      <h1>Pricing & Rates</h1>
+      <p>Configure legitimate service rates and margins.</p>
+    </main>
+  );
+}
