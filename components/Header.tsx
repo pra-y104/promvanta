bc330914a603b1fbabc679c5f0679ba178f40b6c
pra-y104 <lordsprayeromoarelojie@gmail.com>
2026-09-13 20:06:50 +0100

    Create Header component with navigation links

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <strong>PROMVANTA</strong>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/campaigns">Campaigns</a>
+        <a href="/support">Support</a>
+      </nav>
+    </header>
+  );
+}
