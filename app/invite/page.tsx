1c67a82a711be7c17d0f22fd377dca27ae36b1b9
pra-y104 <lordsprayeromoarelojie@gmail.com>
2026-09-13 20:19:28 +0100

    Create page.tsx

---
+++ app/invite/page.tsx
@@ -0,0 +1,9 @@
+export default function InvitePage() {
+  return (
+    <main>
+      <h1>Invite & Earn</h1>
+      <p>Invite friends with your personal referral link.</p>
+      <button>Copy Referral Link</button>
+    </main>
+  );
+}
