cef700f50c386ad8de422bc361c35f1511b0ce95
pra-y104 <lordsprayeromoarelojie@gmail.com>
2026-09-13 20:16:14 +0100

    Create page.tsx

---
+++ app/create-campaign/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateCampaignPage() {
+  return (
+    <main>
+      <h1>Create Campaign</h1>
+      <p>Choose what you want to promote and your campaign goal.</p>
+      <button>Create Campaign</button>
+    </main>
+  );
+}
